31f72749591399b8349653969503d24ac2b94667
techromas <techromas.programming@gmail.com>
2016-10-10 02:00:58 +0900

    add download function

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var output  = document.getElementById("output");                                
 
 var imgpost = document.getElementById("selectfile");
 var canvas  = document.getElementById("canvas");
+var download    = document.getElementById("download");
+var saveButton  = document.getElementById("save");
 
 imgpost.addEventListener("change",function(evt){
     output.innerHTML += "img uploaded <br>";                                    // console
@@ -25,15 +27,27 @@ imgpost.addEventListener("change",function(evt){
             output.innerHTML    += "resizeWidth  = " + resizeWidth + "<br>";    // console
             output.innerHTML    += "resizeHeight = " + resizeHeight + "<br>";   // console
 
-            resizeBuffer.width  = resizeWidth;
-            resizeBuffer.height = resizeHeight;
-            resizeBufferContext.drawImage(img,0,0,resizeWidth,resizeHeight);
-            var resizedImage    = resizeBuffer.toDataURL();
-            output.innerHTML    += "<img src='" + resizedImage + "'>";          // console
+            resizeBuffer.width  = resizeWidth;                          //canvasの幅を指定
+            resizeBuffer.height = resizeHeight;                         //canvasの高さを指定
+            resizeBufferContext.drawImage(img,0,0,resizeWidth,resizeHeight);    //canvasのサイズに合わせて画像を描画
+            
+            var resizedImage    = resizeBuffer.toDataURL('image/png');  //canvasの中身を書き出す
+            var binary          = makeBinary(resizedImage);             //書き出した中身をバイナリに変換
+            var imageUrl        = window.URL.createObjectURL(binary);   //バイナリのURLを取得
+            download.download   = "romas.png";                          //aタグにダウンロード属性をつけてファイル名を指定
+            download.href       = imageUrl;                             //aタグのリンク先を設定
+            //output.innerHTML    += imageUrl + "<br>";                         // console
+            output.innerHTML    += "<img src='" + resizedImage + "'><br>";      // console
         }
         img.src = reader.result;                // 読み込んだ画像をimgに渡している。この後にonloadが処理される
     };
 
+},false);
+
+saveButton.addEventListener("click",function(){
+    output.innerHTML    += "button clicked<br>";
+    //var download    = document.getElementById("download");
+    download.click();
 },false);
 
 };
@@ -46,4 +60,16 @@ var sizeexp = function(origin){
         tmp *= 2;
     }
     return tmp;
-}; 
+};
+
+var makeBinary  = function(base){
+    var bin = atob(base.replace(/^.*,/,''));
+    var buffer  = new Uint8Array(bin.length);
+    for(var i=0;i<bin.length;i++){
+        buffer[i] = bin.charCodeAt(i);
+    }
+    var blob    = new Blob([buffer.buffer],{
+        type: "image/png"
+    });
+    return blob;
+}
